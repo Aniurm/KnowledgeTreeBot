a2f632e269ba12cab886046a98805d3d46363520
Aniurm <534147085@qq.com>
2023-04-30 14:18:52 +0800

    Delete unrelated staff

--- internal/init.go
+++ internal/init.go
@@ -2,12 +2,10 @@ package internal
 
 import (
 	eventHandler "xlab-feishu-robot/internal/event_handler"
-	"xlab-feishu-robot/internal/router"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Init(r *gin.Engine) {
 	eventHandler.Init()
-	router.Register(r)
 }
