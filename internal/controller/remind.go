51efddd0c7835a86fa03ed9e8d80f9b9ae6559c0
Aniurm <534147085@qq.com>
2023-05-02 15:20:34 +0800

    feat: Remind on every month's first day

--- internal/controller/remind.go
+++ internal/controller/remind.go
@@ -1,5 +1,24 @@
 package controller
 
-func Remind() {
+import (
+	"github.com/YasyaKarasu/feishuapi"
+	"github.com/robfig/cron/v3"
+	"xlab-feishu-robot/internal/config"
+	"xlab-feishu-robot/internal/pkg"
+)
 
+const (
+	remindPersonInCharge    = "请及时创建本月的维护记录"
+	remindGroupMembersStart = "请及时开始写本月的知识树文档"
+)
+
+func Remind() {
+	cronTimer := cron.New()
+	// Remind the person in charge to create maintenance record
+	// Remind group members to start writing knowledge tree documents
+	// every month on the 1st at 10:00
+	cronTimer.AddFunc("0 10 1 * *", func() {
+		pkg.Cli.MessageSend(feishuapi.UserUserId, config.C.Info.PersonInChargeID, feishuapi.Text, remindPersonInCharge)
+		pkg.Cli.MessageSend(feishuapi.GroupChatId, config.C.Info.GroupID, feishuapi.Text, remindGroupMembersStart)
+	})
 }
